6201ec86f87cd14432c9319df0bd5fc4955cfe4a
fALKENdk <kasper@friischristensen.com>
2021-09-13 23:14:53 +0200

    Init PopularSearches

---
+++ src/components/PopularSearches/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PopularSearches = () => {
+    const [popularSearches, setPopularSearches] = useState([]);
+
+    useEffect(async () => {
+        const split = 4, splitRresult = [];
+        let i, j, row;
+
+        const fetchData = async () => {
+            const result = await axios(
+                '/assets/popular-searches.json',
+            );
+
+            for (i = 0,j = result.data.length; i < j; i += split) {
+                row = result.data.slice(i, i + split);
+                splitRresult.push(row);
+            }
+
+            setPopularSearches(splitRresult)
+        };
+     
+        fetchData();
+    }, []);
+
+    return (
+        <React.Fragment>
+            <div className="row d-none d-lg-flex">
+                <div className="col">
+                    <h1>PopularSearches</h1>
+                </div>
+            </div>
+
+            <div className="row">
+                {popularSearches.map((row, index) => (
+                    <div className="col" key={`searchgroup-${index}`}>
+                        <ol>
+                            {row.map((searchItem, index) => (
+                                <li key={`searchitem-${index}`}>
+                                    <a href={searchItem.url}>{searchItem.name}</a>
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
+                ))}
+            </div>
+        </React.Fragment>
+    )
+};
+
+export default PopularSearches;
